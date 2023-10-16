d9d029d880a58d963cc1a1a0050e72eab1b26171
StenlyAndika <stenly.andika@gmail.com>
2023-10-16 15:49:44 +0700

    update UI

--- import.php
+++ import.php
@@ -7,6 +7,150 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Reader\Csv;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
+if (isset($_POST['submit_urusan'])) {
+
+    $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    if(isset($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_mimes)) {
+
+        $arr_file = explode('.', $_FILES['file']['name']);
+        $extension = end($arr_file);
+
+        if('csv' == $extension) {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        } else {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        }
+
+        $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+
+        $sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+        if (!empty($sheetData)) {
+            for ($i = 1; $i < count($sheetData); $i++) {
+                if (!empty($sheetData[$i][0]) && empty($sheetData[$i][1])) {
+                    $urusan = $sheetData[$i][0];
+                    $nomenklatur = $sheetData[$i][5];
+                    $sql = "INSERT INTO urusan VALUES('', '$urusan', '$nomenklatur')";
+                    if (mysqli_query($conn, $sql)) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (isset($_POST['submit_bidang_urusan'])) {
+
+    $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    if(isset($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_mimes)) {
+
+        $arr_file = explode('.', $_FILES['file']['name']);
+        $extension = end($arr_file);
+
+        if('csv' == $extension) {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        } else {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        }
+
+        $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+
+        $sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+        if (!empty($sheetData)) {
+            for ($i = 1; $i < count($sheetData); $i++) {
+                if (!empty($sheetData[$i][0]) && !empty($sheetData[$i][1]) && empty($sheetData[$i][2])) {
+                    $bidang_urusan = $sheetData[$i][1];
+                    $nomenklatur = $sheetData[$i][5];
+                    $sql = "INSERT INTO bidang_urusan VALUES('', '$bidang_urusan', '$nomenklatur')";
+                    if (mysqli_query($conn, $sql)) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (isset($_POST['submit_program'])) {
+
+    $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    if(isset($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_mimes)) {
+
+        $arr_file = explode('.', $_FILES['file']['name']);
+        $extension = end($arr_file);
+
+        if('csv' == $extension) {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        } else {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        }
+
+        $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+
+        $sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+        if (!empty($sheetData)) {
+            for ($i = 1; $i < count($sheetData); $i++) {
+                if (!empty($sheetData[$i][0]) && !empty($sheetData[$i][1]) && !empty($sheetData[$i][2]) && empty($sheetData[$i][3])) {
+                    $program = $sheetData[$i][2];
+                    $nomenklatur = $sheetData[$i][5];
+                    $sql = "INSERT INTO program VALUES('', '$program', '$nomenklatur')";
+                    if (mysqli_query($conn, $sql)) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (isset($_POST['submit_kegiatan'])) {
+
+    $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+    if(isset($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_mimes)) {
+
+        $arr_file = explode('.', $_FILES['file']['name']);
+        $extension = end($arr_file);
+
+        if('csv' == $extension) {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+        } else {
+            $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        }
+
+        $spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+
+        $sheetData = $spreadsheet->getActiveSheet()->toArray();
+
+        if (!empty($sheetData)) {
+            for ($i = 1; $i < count($sheetData); $i++) {
+                if (!empty($sheetData[$i][0]) && !empty($sheetData[$i][1]) && !empty($sheetData[$i][2]) && !empty($sheetData[$i][3]) && empty($sheetData[$i][4])) {
+                    $kegiatan = $sheetData[$i][3];
+                    $nomenklatur = $sheetData[$i][5];
+                    $sql = "INSERT INTO kegiatan VALUES('', '$kegiatan', '$nomenklatur')";
+                    if (mysqli_query($conn, $sql)) {
+                        echo "New record created successfully";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                }
+            }
+        }
+    }
+}
+
 if (isset($_POST['submit'])) {
 
     $file_mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
@@ -27,7 +171,7 @@ if (isset($_POST['submit'])) {
         $sheetData = $spreadsheet->getActiveSheet()->toArray();
 
         if (!empty($sheetData)) {
-            for ($i=1; $i<count($sheetData); $i++) {
+            for ($i = 1; $i < count($sheetData); $i++) {
                 if (!empty($sheetData[$i][0])) {
                     $urusan = $sheetData[$i][0];
                     $bidang_urusan = $sheetData[$i][1];
--- index.php
+++ index.php
@@ -1,21 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-        <title>Excel Uploading PHP</title>
-        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Excel Uploading PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1>Excel Upload</h1>
+        <h1 class="text-center">Excel Upload</h1>
         <form method="POST" action="import.php" enctype="multipart/form-data">
-            <div class="form-group">
-                <label>Upload Excel File</label>
-                <input type="file" name="file" class="form-control">
+            <div class="form-group row d-flex justify-content-center align-items-center">
+                <div class="col-9">
+                    <input type="file" name="file" class="form-control">
+                </div>
             </div>
-            <div class="form-group">
-                <button type="submit" name="submit" class="btn btn-success">Upload</button>
+            <div class="form-group mt-2 text-center">
+                <button type="submit" name="submit_urusan" class="btn btn-primary">Upload Urusan</button>
+                <button type="submit" name="submit_bidang_urusan" class="btn btn-secondary">Upload Bidang Urusan</button>
+                <button type="submit" name="submit_program" class="btn btn-success">Upload Program</button>
+                <button type="submit" name="submit_kegiatan" class="btn btn-danger">Upload Kegiatan</button>
+                <button type="submit" name="submit" class="btn btn-warning">Upload Sub Kegiatan</button>
             </div>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 </html>
